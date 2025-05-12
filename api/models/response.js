a5abbd73f4ad39bcb72f2126040cd4c50ef9e402
LopezJLD <95316373+LopezJLD@users.noreply.github.com>
2025-05-11 21:49:24 -0400

    Add files via upload

--- api/models/response.js
+++ api/models/response.js
@@ -1,9 +1,25 @@
 class Response {
-    constructor(status = false, code = 400, message = "", data = null) {
-        this.status = status;
+    constructor() {
+        this.status = false;
+        this.code = 400;
+        this.message = "";
+        this.data = null;
+    }
+
+    success(code, message, data = null) {
+        this.status = true;
         this.code = code;
         this.message = message;
         this.data = data;
+        return this;
+    }
+
+    failure(code, message, error = null) {
+        this.status = false;
+        this.code = code;
+        this.message = message;
+        if (error) this.error = error;
+        return this;
     }
 }
 
